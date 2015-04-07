fb2ae02ad86fc5a6aac3161d53338c2a0ebdf1c7
Hilton Campbell <hiltonc@ldschurch.org>
2015-04-07 09:48:32 -0600

    Remove non-visible table view cells so that they don't stick around.
    Finding accessibility elements in a table view results in cells being
    instantiated and added as subviews of the table view, despite not being
    visible. When the table view has thousands of rows with heavyweight
    cells this can cause severe memory pressure. This commit adds logic to
    explicitly remove these non-visible cells.

--- Additions/UIView-KIFAdditions.m
+++ Additions/UIView-KIFAdditions.m
@@ -169,6 +169,26 @@ NS_INLINE BOOL StringsMatchExceptLineBreaks(NSString *expected, NSString *actual
         [elementStack removeLastObject];
 
         BOOL elementMatches = matchBlock(element);
+        
+        // Remove non-visible table view cells so that they don't stick around
+        if ([element isKindOfClass:NSClassFromString(@"UITableViewCellAccessibilityElement")]) {
+            SEL tableViewCellSelector = NSSelectorFromString(@"tableViewCell");
+            if ([element respondsToSelector:tableViewCellSelector]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+                UITableViewCell *cell = [(id)element performSelector:tableViewCellSelector];
+#pragma clang diagnostic pop
+                
+                UIView *containerView = [UIAccessibilityElement viewContainingAccessibilityElement:element];
+                if ([containerView isKindOfClass:[UITableView class]]) {
+                    UITableView *tableView = (UITableView *)containerView;
+                    
+                    if (![[tableView indexPathsForVisibleRows] containsObject:[tableView indexPathForCell:cell]]) {
+                        [cell removeFromSuperview];
+                    }
+                }
+            }
+        }
 
         if (elementMatches) {
             UIView *viewForElement = [UIAccessibilityElement viewContainingAccessibilityElement:element];
